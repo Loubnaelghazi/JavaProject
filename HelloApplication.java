a82b3804b40308881f6f5ca96a0525463463941e
Loubnaelghazi <114692135+Loubnaelghazi@users.noreply.github.com>
2023-04-01 07:48:18 +0200

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,10 +1,10 @@
 package com.example.esalaf;
 
-import javafx.scene.Scene;
-
-import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 //interfcae de log in
@@ -14,8 +14,8 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml")); //ouvrir hello.fxml
-        Scene scene = new Scene(fxmlLoader.load()); //taille du screen
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml")); //ouvrir hello.fxml
+        Scene scene = new Scene(root); //taille du screen
         stage.setTitle("ESSALAF"); //Nom d application
         stage.setScene(scene);
         stage.show();
